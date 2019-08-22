6a180bf0e2b192b03936c023c4175276bacb899a
Dr.Monkey <monkey@email.com>
2019-08-22 15:08:07 +0800

    new BaseUtils and BaseController from master

---
+++ src/main/java/com/gitdemo/controller/BaseController.java
@@ -0,0 +1,5 @@
+package com.gitdemo.controller;
+
+public class BaseController {
+
+}
--- src/main/java/com/gitdemo/controller/TestController.java
+++
@@ -1,7 +0,0 @@
-package com.gitdemo.controller;
-
-public class TestController {
-	public static void main(String[] args) {
-		System.out.println("来自分支test");
-	}
-}
